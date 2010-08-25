c6935c80c5b58b239bb234090a86596e9cbe6032
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-08-25 15:17:14 +0200

    [develop] Bump up all version numbers, unless something really severe pops up there wont be any more RCs for 1.2.2

--- root/install/install_data.php
+++ root/install/install_data.php
@@ -131,4 +131,5 @@ $versions = array(
 	'1.2.1'		=> array(),	// No database changes
 	'1.2.2-b1'	=> array(), // No database changes
 	'1.2.2-rc1'	=> array(), // No database changes
+	'1.2.2'		=> array(), // No database changes
 );
